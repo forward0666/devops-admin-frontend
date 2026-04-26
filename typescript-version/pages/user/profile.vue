39125e90c875d38d8e327091da0919216d9a02f7
forward <yangyangmyo1994@gmail.com>
2026-04-27 05:25:28 +0800

    fix: userConsoleService 改为 userConsoleProfileService

--- typescript-version/pages/user/profile.vue
+++ typescript-version/pages/user/profile.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { userConsoleService } from '~/services/api'
+import { userConsoleProfileService } from '~/services/api'
 const authStore = useAuthStore()
 const userStore = useUserStore()
 const snackbar = ref({ show: false, text: '', color: 'success' })
@@ -61,7 +61,7 @@ async function changePassword() {
     return
   }
   try {
-    const res: any = await userConsoleService.changePassword({ oldPassword: oldPassword.value, newPassword: newPassword.value })
+    const res: any = await userConsoleProfileService.changePassword({ oldPassword: oldPassword.value, newPassword: newPassword.value })
     oldPassword.value = ''
     newPassword.value = ''
     confirmPassword.value = ''
