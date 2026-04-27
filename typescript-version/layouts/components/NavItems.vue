f86e19c5adf91421de5efc8c3d82b9ed62a62c52
forward <yangyangmyo1994@gmail.com>
2026-04-28 05:01:55 +0800

    fix: 导航Settings改为Setting

--- typescript-version/layouts/components/NavItems.vue
+++ typescript-version/layouts/components/NavItems.vue
@@ -132,7 +132,7 @@ const isProjectActive = (projectId: number) => {
       <VDivider class="my-2 mx-3" />
       <VerticalNavLink
         :item="{
-          title: 'Settings',
+          title: 'Setting',
           icon: 'bx-cog',
           to: '/admin/monitor/setting',
         }"
