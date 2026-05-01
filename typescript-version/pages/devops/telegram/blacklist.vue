791b7f44da9fbb0c5378a6cb19922267a43a9e3b
forward <yangyangmyo1994@gmail.com>
2026-05-02 06:43:38 +0800

    fix: 解锁图标改为tabler-lock-open-2

--- typescript-version/pages/devops/telegram/blacklist.vue
+++ typescript-version/pages/devops/telegram/blacklist.vue
@@ -113,7 +113,7 @@ onMounted(async () => {
           <VTooltip text="Remove from Blacklist">
             <template #activator="{ props }">
               <VBtn v-bind="props" icon variant="text" color="warning" size="small" @click="removeBlacklist(item)">
-                <VIcon icon="bx-lock-open" />
+                <VIcon icon="tabler-lock-open-2" />
               </VBtn>
             </template>
           </VTooltip>
