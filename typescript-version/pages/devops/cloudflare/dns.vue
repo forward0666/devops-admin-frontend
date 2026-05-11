aa55e10101c922fb4caa61ebf40f625f43032101
forward <yangyangmyo1994@gmail.com>
2026-05-12 00:28:16 +0800

    fix: separate toolbar and data, scroll only data area

--- typescript-version/pages/devops/cloudflare/dns.vue
+++ typescript-version/pages/devops/cloudflare/dns.vue
@@ -244,9 +244,9 @@ function exportCSV() {
       </VCardText>
     </VCard>
 
-    <VCard v-if="selectedAccountId">
+    <VCard v-if="selectedAccountId" style="display: flex; flex-direction: column; max-height: calc(100vh - 140px);">
       <VProgressLinear v-if="loadingRecords" indeterminate color="primary" />
-      <div v-if="sortedRecords.length">
+      <div v-if="sortedRecords.length" style="flex: 1; overflow-y: auto">
         <VTable class="text-no-wrap" hover density="compact" style="table-layout: fixed; width: 100%;">
           <colgroup>
             <col style="width: 250px" />
