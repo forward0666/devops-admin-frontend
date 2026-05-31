891bf06a65ba8354c9efc00c72dea46aa58e3b5a
forward <yangyangmyo1994@gmail.com>
2026-06-01 05:30:51 +0800

    fix: add __dirname for ESM in build-icons.ts

--- typescript-version/plugins/iconify/build-icons.ts
+++ typescript-version/plugins/iconify/build-icons.ts
@@ -15,6 +15,8 @@ import { dirname, join } from 'node:path'
 import { createRequire } from 'node:module'
 import { fileURLToPath } from 'node:url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 const require = createRequire(import.meta.url)
 
 // Installation: npm install --save-dev @iconify/tools @iconify/utils @iconify/json @iconify/iconify
