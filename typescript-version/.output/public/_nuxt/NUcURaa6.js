70746b9870abc0efb23ddadf63720625fe9146d1
forward <yangyangmyo1994@gmail.com>
2026-06-01 06:19:28 +0800

    dist: add pre-built static files

---
+++ typescript-version/.output/public/_nuxt/NUcURaa6.js
@@ -0,0 +1,1 @@
+import{p as t,c0 as e,ad as s,v as n}from"./CDxsAfBQ.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:n(()=>o.value?void 0:{transition:"none !important"}),isBooted:e(o)}}export{i as u};
