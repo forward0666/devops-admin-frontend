9796691f2dca4e426fb6d711611fa09d855dd146
forward <yangyangmyo1994@gmail.com>
2026-06-18 04:02:56 +0800

    fix: restore IT Tools redirect with env var

--- pages/user/tool/ittool.vue
+++ pages/user/tool/ittool.vue
@@ -1,17 +1,27 @@
 <script setup lang="ts">
 definePageMeta({ layout: 'default' })
+
+const itToolsUrl = import.meta.env.VITE_IT_TOOLS_URL
+
+if (itToolsUrl) {
+  window.location.href = itToolsUrl
+}
 </script>
 
 <template>
   <div>
-    <VCard class="pa-8 text-center">
+    <VCard v-if="!itToolsUrl" class="pa-8 text-center">
       <VCardText>
         <VIcon size="48" color="grey" class="mb-4">bx-error</VIcon>
         <h4 class="text-h4 mb-2">IT Tools</h4>
         <p class="text-body-1 text-medium-emphasis">
-          页面加载中，如无法访问请联系管理员
+          未配置 IT Tools 地址，请联系管理员
         </p>
       </VCardText>
     </VCard>
+    <div v-else class="text-center pa-8">
+      <VProgressCircular indeterminate color="primary" />
+      <p class="mt-4">正在跳转到 IT Tools...</p>
+    </div>
   </div>
 </template>
