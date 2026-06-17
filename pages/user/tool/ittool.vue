85c65c52b5733f96622b09ea64e292657faa7f53
forward <yangyangmyo1994@gmail.com>
2026-06-18 04:05:11 +0800

    fix: simplify ittool page - redirect or let nginx handle

--- pages/user/tool/ittool.vue
+++ pages/user/tool/ittool.vue
@@ -9,19 +9,5 @@ if (itToolsUrl) {
 </script>
 
 <template>
-  <div>
-    <VCard v-if="!itToolsUrl" class="pa-8 text-center">
-      <VCardText>
-        <VIcon size="48" color="grey" class="mb-4">bx-error</VIcon>
-        <h4 class="text-h4 mb-2">IT Tools</h4>
-        <p class="text-body-1 text-medium-emphasis">
-          未配置 IT Tools 地址，请联系管理员
-        </p>
-      </VCardText>
-    </VCard>
-    <div v-else class="text-center pa-8">
-      <VProgressCircular indeterminate color="primary" />
-      <p class="mt-4">正在跳转到 IT Tools...</p>
-    </div>
-  </div>
+  <div />
 </template>
