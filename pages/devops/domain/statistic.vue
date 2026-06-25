834bbe7a55e681ce863a8ad38bd399bee311d3cf
forward <yangyangmyo1994@gmail.com>
2026-06-26 07:01:35 +0800

    feat: add back Top 10 by Country chart

--- pages/devops/domain/statistic.vue
+++ pages/devops/domain/statistic.vue
@@ -318,6 +318,12 @@ onMounted(async () => {
           <apexchart type="bar" :options="bandwidthChart.options" :series="bandwidthChart.series" height="180" />
         </VCardText>
       </VCard>
+      <VCard style="flex: 2; min-width: 0;">
+        <VCardTitle class="text-body-2 pa-2">Top 10 by Country</VCardTitle>
+        <VCardText class="pa-1">
+          <apexchart type="bar" :options="countryChart.options" :series="countryChart.series" height="180" />
+        </VCardText>
+      </VCard>
     </div>
 
     <!-- Table -->
